97512e8920b00d2cf98455666402819ddce52bfc
Joseph <frasier.joseph@gmail.com>
2023-03-02 11:59:30 -0600

    change strings in conditionals to variables
    changing the strings inside of the conditionals for the items to be variables as it would be easier to change their logic and less prone to having problems in our strings when we type them

--- TypeScript/app/rose-garden.ts
+++ TypeScript/app/rose-garden.ts
@@ -10,6 +10,10 @@ export class Item {
   }
 }
 
+const BRIE: string = "Aged Brie";
+const BACKSTAGE_PASS: string = "Backstage passes to a TAFKAL80ETC concert";
+const HAND: string = "Sulfuras, Hand of Ragnaros";
+
 export class RoseGarden {
   items: Array<Item>;
 
@@ -19,46 +23,47 @@ export class RoseGarden {
 
   updateQuality() {
     for (let i = 0; i < this.items.length; i++) {
-      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+      if (this.items[i].name != BRIE && this.items[i].name != BACKSTAGE_PASS) {
         if (this.items[i].quality > 0) {
-          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-            this.items[i].quality = this.items[i].quality - 1
+          if (this.items[i].name != HAND) {
+            this.items[i].quality = this.items[i].quality - 1;
           }
         }
       } else {
         if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1
-          if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
+          this.items[i].quality = this.items[i].quality + 1;
+          if (this.items[i].name == BACKSTAGE_PASS) {
             if (this.items[i].sellIn < 11) {
               if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1
+                this.items[i].quality = this.items[i].quality + 1;
               }
             }
             if (this.items[i].sellIn < 6) {
               if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1
+                this.items[i].quality = this.items[i].quality + 1;
               }
             }
           }
         }
       }
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+      if (this.items[i].name != HAND) {
         this.items[i].sellIn = this.items[i].sellIn - 1;
       }
       if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != 'Aged Brie') {
-          if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+        if (this.items[i].name != BRIE) {
+          if (this.items[i].name != BACKSTAGE_PASS) {
             if (this.items[i].quality > 0) {
-              if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].quality = this.items[i].quality - 1
+              if (this.items[i].name != HAND) {
+                this.items[i].quality = this.items[i].quality - 1;
               }
             }
           } else {
-            this.items[i].quality = this.items[i].quality - this.items[i].quality
+            this.items[i].quality =
+              this.items[i].quality - this.items[i].quality;
           }
         } else {
           if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1
+            this.items[i].quality = this.items[i].quality + 1;
           }
         }
       }
