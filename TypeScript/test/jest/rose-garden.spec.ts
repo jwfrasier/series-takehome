1ebb89b2fd98f0f6eca31c24f036e05fdccf7e1c
Joseph <frasier.joseph@gmail.com>
2023-03-02 11:55:58 -0600

    initial test to just see get jest working and passing my test
    might need to recompile the ts when i write my test to have the updated js file. test is passing for updatedQuality

--- TypeScript/test/jest/rose-garden.spec.ts
+++ TypeScript/test/jest/rose-garden.spec.ts
@@ -1,9 +1,17 @@
-import { Item, RoseGarden } from '@/rose-garden';
+import { Item, RoseGarden } from "@/rose-garden";
 
-describe('Rose Garden', () => {
-  it('should foo', () => {
-    const roseGarden = new RoseGarden([new Item('foo', 0, 0)]);
+describe("Rose Garden", () => {
+  it("should foo", () => {
+    const roseGarden = new RoseGarden([new Item("foo", 0, 0)]);
     const items = roseGarden.updateQuality();
-    expect(items[0].name).toBe('fixme');
+    expect(items[0].name).toBe("foo");
+  });
+});
+describe("updateQuality", () => {
+  it("decreases quality by 1 for normal items before sellIn date", () => {
+    const item = new Item("Normal", 10, 10);
+    const roseGarden = new RoseGarden([item]);
+    roseGarden.updateQuality();
+    expect(item.quality).toBe(9);
   });
 });
