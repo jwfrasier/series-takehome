ae0808527cf375655d820abcebb0803264d37f19
Joseph <frasier.joseph@gmail.com>
2023-03-02 12:18:03 -0600

    test cases for brie, hand, and backstage passes
    test cases for the following based on the readme
    also made variables instead of the hardcoded string names
    - Once the sell by date has passed, Quality degrades twice as fast
    - The Quality of an item is never negative
    - "Aged Brie" actually increases in Quality the older it gets
    - The Quality of an item is never more than 50
    - "Sulfuras", being a legendary item, never has to be sold or decreases in Quality
    - "Backstage passes", like aged brie, increases in Quality as its SellIn value approaches;
    - Quality increases by 2 when there are 10 days or less and by 3 when there are 5 days or less but
    - Quality drops to 0 after the concert

--- TypeScript/test/jest/rose-garden.spec.ts
+++ TypeScript/test/jest/rose-garden.spec.ts
@@ -1,5 +1,9 @@
 import { Item, RoseGarden } from "@/rose-garden";
 
+const BRIE: string = "Aged Brie";
+const BACKSTAGE_PASSES: string = "Backstage passes to a TAFKAL80ETC concert";
+const HAND: string = "Sulfuras, Hand of Ragnaros";
+
 describe("Rose Garden", () => {
   it("should foo", () => {
     const roseGarden = new RoseGarden([new Item("foo", 0, 0)]);
@@ -9,9 +13,66 @@ describe("Rose Garden", () => {
 });
 describe("updateQuality", () => {
   it("decreases quality by 1 for normal items before sellIn date", () => {
-    const item = new Item("Normal", 10, 10);
+    const item = new Item("Normal Item", 10, 10);
     const roseGarden = new RoseGarden([item]);
     roseGarden.updateQuality();
     expect(item.quality).toBe(9);
+  });
+
+  it("decrease the quality by 2 if normal items are past their sellIn date", () => {
+    const item = new Item("Normal Item", 0, 10);
+    const roseGarden = new RoseGarden([item]);
+    roseGarden.updateQuality();
+    expect(item.quality).toBe(8);
+  });
+  it("should not decrease the quality value of a normal item below 0", () => {
+    const item = new Item("Normal Item", 0, 0);
+    const garden = new RoseGarden([item]);
+    garden.updateQuality();
+    expect(item.sellIn).toBe(-1);
+    expect(item.quality).toBe(0);
+  });
+  it("should not increase Aged Brie above a quality of 50", () => {
+    const item = new Item(BRIE, 5, 50);
+    const garden = new RoseGarden([item]);
+    garden.updateQuality();
+    expect(item.sellIn).toBe(4);
+    expect(item.quality).toBe(50);
+  });
+  it("should increase quality of Aged Brie the older it gets", () => {
+    const item = new Item(BRIE, 5, 30);
+    const garden = new RoseGarden([item]);
+    garden.updateQuality();
+    expect(item.sellIn).toBe(4);
+    expect(item.quality).toBe(31);
+  });
+  it("increases the quality of Backstage passes by 2 with 10 days or less to the concert", () => {
+    const item = new Item(BACKSTAGE_PASSES, 9, 10);
+    const roseGarden = new RoseGarden([item]);
+    roseGarden.updateQuality();
+    expect(item.sellIn).toEqual(8);
+    expect(item.quality).toEqual(12);
+  });
+
+  it("increases the quality of Backstage passes by 3 with 5 days or less to the concert", () => {
+    const item = new Item(BACKSTAGE_PASSES, 5, 10);
+    const roseGarden = new RoseGarden([item]);
+    roseGarden.updateQuality();
+    expect(item.sellIn).toEqual(4);
+    expect(item.quality).toEqual(13);
+  });
+  it("sets the quality of Backstage passes to 0 after the concert", () => {
+    const item = new Item(BACKSTAGE_PASSES, 0, 10);
+    const roseGarden = new RoseGarden([item]);
+    roseGarden.updateQuality();
+    expect(item.sellIn).toEqual(-1);
+    expect(item.quality).toEqual(0);
+  });
+  it("should not decrease the quality or sellIn of Sulfuras", () => {
+    const sulfuras = new Item(HAND, 10, 80);
+    const roseGarden = new RoseGarden([sulfuras]);
+    roseGarden.updateQuality();
+    expect(sulfuras.sellIn).toEqual(10);
+    expect(sulfuras.quality).toEqual(80);
   });
 });
