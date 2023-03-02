27ee22dbfd87cb3a466044ed04afa71fe3de5240
Joseph <frasier.joseph@gmail.com>
2023-03-02 12:01:52 -0600

    old for loop to for..of loop
    change for loop to a for..of loop so that way we can get rid of the this.items[i] parts in the code.  Clearer and less cluttered to read

--- TypeScript/app/rose-garden.ts
+++ TypeScript/app/rose-garden.ts
@@ -22,51 +22,47 @@ export class RoseGarden {
   }
 
   updateQuality() {
-    for (let i = 0; i < this.items.length; i++) {
-      if (
-        this.items[i].name != BRIE &&
-        this.items[i].name != BACKSTAGE_PASSES
-      ) {
-        if (this.items[i].quality > 0) {
-          if (this.items[i].name != HAND) {
-            this.items[i].quality = this.items[i].quality - 1;
+    for (const item of this.items) {
+      if (item.name != BRIE && item.name != BACKSTAGE_PASSES) {
+        if (item.quality > 0) {
+          if (item.name != HAND) {
+            item.quality = item.quality - 1;
           }
         }
       } else {
-        if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1;
-          if (this.items[i].name == BACKSTAGE_PASSES) {
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
+        if (item.quality < 50) {
+          item.quality = item.quality + 1;
+          if (item.name == BACKSTAGE_PASSES) {
+            if (item.sellIn < 11) {
+              if (item.quality < 50) {
+                item.quality = item.quality + 1;
               }
             }
-            if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
+            if (item.sellIn < 6) {
+              if (item.quality < 50) {
+                item.quality = item.quality + 1;
               }
             }
           }
         }
       }
-      if (this.items[i].name != HAND) {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
+      if (item.name != HAND) {
+        item.sellIn = item.sellIn - 1;
       }
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != BRIE) {
-          if (this.items[i].name != BACKSTAGE_PASSES) {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name != HAND) {
-                this.items[i].quality = this.items[i].quality - 1;
+      if (item.sellIn < 0) {
+        if (item.name != BRIE) {
+          if (item.name != BACKSTAGE_PASSES) {
+            if (item.quality > 0) {
+              if (item.name != HAND) {
+                item.quality = item.quality - 1;
               }
             }
           } else {
-            this.items[i].quality =
-              this.items[i].quality - this.items[i].quality;
+            item.quality = item.quality - item.quality;
           }
         } else {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1;
+          if (item.quality < 50) {
+            item.quality = item.quality + 1;
           }
         }
       }
